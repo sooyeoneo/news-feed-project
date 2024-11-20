22db4bb4bf98fac733291db031fb6946e3bfb0f6
sooyeon_heo <suzi97@naver.com>
2024-11-20 16:41:13 +0900

    fix : Entity, Dto 기본 코드 수정

--- src/main/java/com/example/newsfeedproject/dto/post/UpdatePostReqDto.java
+++ src/main/java/com/example/newsfeedproject/dto/post/UpdatePostReqDto.java
@@ -5,5 +5,12 @@ import lombok.Getter;
 @Getter
 public class UpdatePostReqDto {
 
+    private final String title;
 
+    private final String contents;
+
+    public UpdatePostReqDto(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
 }
--- src/main/java/com/example/newsfeedproject/entity/BaseEntity.java
+++ src/main/java/com/example/newsfeedproject/entity/BaseEntity.java
@@ -1,15 +1,18 @@
 package com.example.newsfeedproject.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.hibernate.boot.jaxb.internal.stax.LocalSchemaLocator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
+@MappedSuperclass  //매핑정보만 상속받는 superclass라는 의미
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
