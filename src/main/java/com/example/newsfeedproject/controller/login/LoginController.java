75b00db1f8fae195c2a7e4932aa5a5ca11a3c9b2
sooyeon_heo <suzi97@naver.com>
2024-11-21 11:44:55 +0900

    feat : 로그인 Controller, Service, Dto, Repository 생성

---
+++ src/main/java/com/example/newsfeedproject/controller/login/LoginController.java
@@ -0,0 +1,47 @@
+package com.example.newsfeedproject.controller.login;
+
+import com.example.newsfeedproject.dto.login.LoginRequestDto;
+import com.example.newsfeedproject.dto.login.LoginResponseDto;
+import com.example.newsfeedproject.dto.user.UserResponseDto;
+import com.example.newsfeedproject.service.login.LoginServiceImpl;
+import com.example.newsfeedproject.service.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final UserService userService;
+    private final LoginServiceImpl loginServiceImpl;
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequestDto loginRequestDto, HttpServletRequest servletRequest) {
+        LoginResponseDto loginResponseDto = loginServiceImpl.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+        String email = loginResponseDto.getUserName();
+
+        HttpSession httpSession = servletRequest.getSession();
+        LoginResponseDto loginUser = loginServiceImpl.findUserByEmail(email);
+        httpSession.setAttribute("LOGIN_USER", loginUser);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest servletRequest) {
+        HttpSession httpSession = servletRequest.getSession(false);
+        if (httpSession != null) {
+            httpSession.invalidate();
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/example/newsfeedproject/service/login/LoginServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.newsfeedproject.service.login;
+
+import com.example.newsfeedproject.config.PasswordEncoder;
+import com.example.newsfeedproject.dto.login.LoginResponseDto;
+import com.example.newsfeedproject.entity.user.User;
+import com.example.newsfeedproject.repository.login.LoginRepository;
+import com.example.newsfeedproject.repository.post.PostRepository;
+import com.example.newsfeedproject.repository.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginServiceImpl implements LoginService {
+
+    private final LoginRepository loginRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public LoginResponseDto findUserByEmail(String email) {
+
+        Optional<User> optionalUser = loginRepository.findUserByEmail(email);
+        if (optionalUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 email 입니다. ");
+        }
+        User findUser = optionalUser.get();
+        return new LoginResponseDto(findUser.getUserName());
+    }
+
+    // 비밀번호 일치 여부 확인
+    @Transactional
+    public void matchPassword(String email, String password) {
+
+        User user = loginRepository.findUserByEmailOrElseThrow(email);
+        if (!user.getPassword().equals(password)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀 번호가 일치하지 않습니다.");
+        }
+    }
+
+    // 유저 로그인
+    @Override
+    public LoginResponseDto login(String email, String password) {
+        User findUser = loginRepository.findUserByEmailOrElseThrow(email);
+        if (!passwordEncoder.matches(password, findUser.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀 번호가 일치하지 않습니다.");
+        }
+        return new LoginResponseDto(findUser.getEmail());
+    }
+}
